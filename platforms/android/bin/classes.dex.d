3b61d3cf503089376efae384432dacabfd2faf6f
gdmec07120721 <704123013@qq.com>
2014-04-23 11:59:41 +0800

    07120721

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\capture\platforms\android\bin\classes.dex : \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\BuildConfig.class \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\Mycapture.class \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\R$attr.class \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\R$drawable.class \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\R$string.class \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\R$xml.class \
+C:\capture\platforms\android\bin\classes\cn\edu\gdmec\x07120721\capture\R.class \
+C:\capture\platforms\android\bin\dexedLibs\classes-c130c2eb36d3fac1cad34418099e2596.jar \
